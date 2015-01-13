9966d3ae57d393a85b7ac72a8da1171b4338068e
peteryeon <bigbigba1@163.com>
2015-01-13 20:22:19 +0800

    稍微整理一下，继续上路

--- src/main/java/com/gudashi/service/entity/UserStockPK.java
+++ src/main/java/com/gudashi/service/entity/UserStockPK.java
@@ -7,7 +7,7 @@ import javax.persistence.Embeddable;
 
 @Embeddable
 public class UserStockPK implements Serializable {
-	private static final long serialVersionUID = -6353664682579298588L;
+    private static final long serialVersionUID = -6353664682579298588L;
 
 	@Column(name="user_id")
 	private String userId;
@@ -34,4 +34,35 @@ public class UserStockPK implements Serializable {
 	public void setStockId(String stockId) {
 		this.stockId = stockId;
 	}
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((stockId == null) ? 0 : stockId.hashCode());
+        result = prime * result + ((userId == null) ? 0 : userId.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        UserStockPK other = (UserStockPK) obj;
+        if (stockId == null) {
+            if (other.stockId != null)
+                return false;
+        } else if (!stockId.equals(other.stockId))
+            return false;
+        if (userId == null) {
+            if (other.userId != null)
+                return false;
+        } else if (!userId.equals(other.userId))
+            return false;
+        return true;
+    }
 }
